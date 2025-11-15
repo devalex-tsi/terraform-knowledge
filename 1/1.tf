314cce4b46aad687700dbd544106f9a5fd233b2f
Alex <alex.tsi.dev@gmail.com>
2025-11-15 22:52:10 +0100

    aws_instance was added, gitignore was extended

--- 1/1.tf
+++ 1/1.tf
@@ -5,6 +5,12 @@ provider "aws" {
 }
 
 resource "aws_instance" "ubuntu-instance" {
+  count         = 2
   ami           = "ami-004e960cde33f9146"
+  instance_type = "t3.micro"
+}
+
+resource "aws_instance" "aws-ubuntu-instance" {
+  ami           = "ami-089a7a2a13629ecc4"
   instance_type = "t3.micro"
 }
